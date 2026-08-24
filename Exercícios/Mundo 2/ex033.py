f6b01d90e509b6d63c060dd717994bcce9969103
oguhperes <gustavo2009peres@gmail.com>
2026-08-24 09:46:39 -0300

    Atualizações no exercícios 033

--- Exercícios/Mundo 2/ex033.py
+++ Exercícios/Mundo 2/ex033.py
@@ -2,22 +2,28 @@
 # Se ele vai se alistar futuramente, se é a hora de se alistar, se ja passou do tempo de se alistar, mostrar também o tempo
 # que falta ou passou do prazo
 
+
 try:
     print(' \n--- Consultar seu status de serviço militar --- ')
     ano = int(input('Em que ano você nasceu? '))
     idade = 2026 - ano
 
-    if idade < 18:
-        anos_faltando = 18 - idade
-        print(f'Você não precisa se alistar! Faltam {anos_faltando} Ano(s) para você se alistar ')
-
-    elif idade == 18:
-        print('Está na hora de se alistar! Acesse o site https://alistamento.eb.mil.br/')
+    if ano > 2026:
+        print('Digite um ano válido! ')
 
     else:
-        anos_passados_do_prazo = idade - 18
-        print(f'Já passou a hora de se alistar! Você está {anos_passados_do_prazo} Ano(s) atrasado ')
-    
+
+        if idade < 18:
+            anos_faltando = 18 - idade
+            print(f'Você não precisa se alistar! Faltam {anos_faltando} Ano(s) para você se alistar ')
+
+        elif idade == 18:
+            print('Está na hora de se alistar! Acesse o site https://alistamento.eb.mil.br/')
+
+        else:
+            anos_passados_do_prazo = idade - 18
+            print(f'Já passou a hora de se alistar! Você está {anos_passados_do_prazo} Ano(s) atrasado ')
+        
 
 except ValueError:
     print('Digite um número válido! ')
